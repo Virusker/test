b2ec97290daa8d620035088a7b5fdac12f3a52e2
Virusker <69068783+Virusker@users.noreply.github.com>
2024-02-23 21:12:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "oktét 11aaaa1"
-print() 
+    return "oktét 11aaaa15"
+print()
 print()
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=80)
