8590a505654c79d47be4bfd42afd59296a6b465e
Phan Thế Khánh <69068783+Virusker@users.noreply.github.com>
2024-06-08 20:28:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, abort
 import os
 from dotenv import load_dotenv
 app = Flask(__name__)
-<<<<<<< HEAD
 
 load_dotenv(".env")
 
@@ -10,15 +9,6 @@ load_dotenv(".env")
 @app.route("/")
 def index():
     v = os.environ.get("A")
-=======
-try:
-   v = os.environ.get('VERCEL_URL')
-except:
-   print("nothing")
-@app.route("/")
-def index():
-   
->>>>>>> b7e49d62930377da4b40c388a1288d5a4f684e52
     if v:
         return f"this var:{v}"
     else:
